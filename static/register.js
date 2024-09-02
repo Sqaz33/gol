9e83bcb946944f97e6d80c08b991e458e134bc44
Степан Матвеев <Matveev Stepan>
2024-09-02 12:41:16 +0300

    add statics for reg

---
+++ static/register.js
@@ -0,0 +1,22 @@
+function login() {
+    const data = {
+        login: document.getElementById("login").value,
+        pword: document.getElementById("pword").value
+    };
+
+    fetch('/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.text())
+    .then(result => {
+        window.alert(result);
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+
+}
