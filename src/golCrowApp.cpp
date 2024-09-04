698be1399868827e26f04a79e0a2d1d22ebb0090
Matveev Stepan Andreevich <mset321@gmail.com>
2024-09-04 20:55:26 +0300

    add secret

--- src/golCrowApp.cpp
+++ src/golCrowApp.cpp
@@ -7,6 +7,12 @@
 
 #include "include/user.hpp"
 
+namespace {
+    std::uint32_t getIPByReq(const crow::request& req) {
+        return boost::asio::ip::make_address_v4(req.remote_ip_address).to_uint();
+    }
+}
+
 namespace gol_crow_app {
 
 void GolCrowApp::run() {
@@ -55,7 +61,7 @@ void GolCrowApp::setupRoutes() {
 
     CROW_ROUTE(app, "/register") 
     .methods(crow::HTTPMethod::Post)([&](const crow::request& req){
-        auto hostIP = boost::asio::ip::make_address_v4(req.remote_ip_address).to_uint();
+        auto hostIP = getIPByReq(req);
         if (hostsIP.count(hostIP)) {
             return "it's ok";
         }
@@ -69,6 +75,18 @@ void GolCrowApp::setupRoutes() {
 
     CROW_CATCHALL_ROUTE(app)([&]() {
         return "nothing here";
+    });
+
+        
+    CROW_ROUTE(app, "/secret")
+    .methods(crow::HTTPMethod::Get)([&](const crow::request& req){
+        crow::response res;
+        if (hostsIP.count(getIPByReq(req))) {
+            res.set_static_file_info("static/secret/secret.png");
+        } else {
+            res.set_static_file_info("static/secret/secret.html");
+        }
+        return res;
     });
 
 }
